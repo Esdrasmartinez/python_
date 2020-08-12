d6539e87c60cb4de636422e923de7353a03f5c6d
Esdrasmartinez <64509830+Esdrasmartinez@users.noreply.github.com>
2020-08-12 10:33:27 -0500

    Update graficado_simple.py

--- graficado_simple.py
+++ graficado_simple.py
@@ -1,19 +1,33 @@
 from bokeh.plotting import figure, output_file, show
- 
+import math
 if __name__ == "__main__":
-    output_file(Graficado_simple.html)
+    output_file('Graficado_simple.html')
     fig = figure()
-    total_values = int(input('Cuantos valores deseas graficar: '))
+    
 
-    x_vals = list(range(total_values))
-
+    x_vals = []
     y_vals = [] 
 
-    for x in x_vals:
-        val = int(input(f'Escoge el valor de y para la posición {x} de las abscisas: '))
+    for x in range(2):
+        val = int(input(f'Escoge el valor de X {x+1} '))
+        x_vals.append(val)
+
+    for y in range(2):
+        val = int(input(f'Escoge el valor de Y{y+1}'))
         y_vals.append(val)
+
+    pendiente = (y_vals[1] - y_vals[0]) / (x_vals[0] - x_vals[1]) 
+    angulo_inclinacion = math.degrees(math.atan(pendiente))
+    
+    print(f"""
+    {y_vals[1]} - {y_vals[0]}
+    --------- = {pendiente}  Ángulo de inclianción = {angulo_inclinacion}
+    {x_vals[1]} - {x_vals[0]}
+       """)
+
+    
 
     fig.line(x_vals,y_vals, line_width = 2)
     show(fig)
-    print('hello world')
+    
     
